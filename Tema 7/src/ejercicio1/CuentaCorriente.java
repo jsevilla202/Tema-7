95b556e0fb7fa84d3887039191025322f2d11568
jsevilla202 <jsevilla@iesnervion.es>
2023-03-28 11:55:16 +0200

    Javadoc

--- Tema 7/src/ejercicio1/CuentaCorriente.java
+++ Tema 7/src/ejercicio1/CuentaCorriente.java
@@ -1,24 +1,49 @@
 package ejercicio1;
-
+/**
+ * Clase con las funciones de Cuenta Corriente
+ * @author Jaime Sevilla
+ *
+ */
 public class CuentaCorriente {
-	String DNI;
-	String nombre;
-	double saldo;
+	/**
+	 * DNI del cliente
+	 */
+	private String DNI;
+	/**
+	 * Nombre del cliente
+	 */
+	private String nombre;
+	/**
+	 * Saldo disponible en la cuenta del cliente
+	 */
+	private double saldo;
 	
-	
+	/**
+	 * Constructor con DNI y Saldo
+	 * @param dNI DNI dado por el usuario
+	 * @param saldo Saldo dado por el usuario
+	 */
 	public CuentaCorriente(String dNI, double saldo) {
 		super();
 		DNI = dNI;
 		this.saldo = saldo;
 	}
-
+	/**
+	 * Constructor con todos los parametros
+	 * @param dNI DNI dado por el usuario
+	 * @param nombre Nombre dado por el usuario
+	 * @param saldo Saldo dado por el usuario
+	 */
 	public CuentaCorriente(String dNI, String nombre, double saldo) {
 		super();
 		DNI = dNI;
 		this.nombre = nombre;
 		this.saldo = saldo;
 	}
-	
+	/**
+	 * Saca una cantidad de dinero del saldo
+	 * @param cantidad Cantidad de dinero a sacar
+	 */
 	void sacarDinero(double cantidad) {
 		if(cantidad>saldo || cantidad<0) {
 			System.out.println("ERROR: No tiene suficiente saldo en la cuenta");
@@ -28,7 +53,10 @@ public class CuentaCorriente {
 			saldo -= cantidad;
 		}
 	}
-	
+	/**
+	 * Ingresa dienro en el saldo
+	 * @param cantidad Cantidad de dinero a ingresar
+	 */
 	void ingresarDinero(double cantidad) {
 		if(cantidad<0) {
 			System.out.println("ERROR: No puede introducir valores negativos");
@@ -38,7 +66,9 @@ public class CuentaCorriente {
 			saldo += cantidad;
 		}
 	}
-	
+	/**
+	 * Muestra el dinero en el banco
+	 */
 	void mostrarDinero() {
 		System.out.println("Sr./Sra. " + nombre +  " con DNI " + DNI + " tiene un saldo de: " + saldo + "€");
 	}
--- Tema 7/src/ejercicio4/Articulo.java
+++ Tema 7/src/ejercicio4/Articulo.java
@@ -1,22 +1,49 @@
 package ejercicio4;
-
+/**
+ * Clase con las funciones de Articulo
+ * @author Jaime Sevilla
+ *
+ */
 public class Articulo {
+	/**
+	 * Nombre del articulo
+	 */
 	private String nombre;
+	/**
+	 * Precio del Articulo
+	 */
 	private double precio;
+	/**
+	 * IVA
+	 */
 	public static final int IVA = 21;
+	/***
+	 * Cantidades restantes del articulo
+	 */
 	private int cuantosQuedan;
 	
+	/**
+	 * Constructor con todos los parametros
+	 * @param nombre Nombre dado por el usuario
+	 * @param precio Precio dado por el usuario
+	 * @param cuantosQuedan Cantidades restantes dadas por el usurio
+	 */
 	public Articulo(String nombre, double precio, int cuantosQuedan) {
 		super();
 		this.nombre = nombre;
 		this.precio = precio;
 		this.cuantosQuedan = cuantosQuedan;
 	}
-	
+	/**
+	 * Imprime los datos del producto
+	 */
 	void imprime() {
 		System.out.println("El producto " + nombre + " tiene un precio de " + (precio+(precio*(IVA/100))) + " y quedan " + cuantosQuedan + " en el almacen");
 	}
-	
+	/**
+	 * Calcula el precio del producto con el IVA incluido
+	 * @return Devuelve el precio con IVA
+	 */
 	double getPVP() {
 		double precioiva;
 		
@@ -24,7 +51,11 @@ public class Articulo {
 		
 		return precioiva;
 	}
-	
+	/**
+	 * Calcula el precio del prudcto con IVA y descuento
+	 * @param descuento Descuento dado por el usuario
+	 * @return Devuelve el Precio del producto con el descuento dado por el usuario
+	 */
 	double getPVPDescuento(double descuento) {
 		double precioiva;
 		double preciodescuento;
@@ -35,7 +66,11 @@ public class Articulo {
 		
 		return preciodescuento;
 	}
-	
+	/**
+	 * Comprueba si se puede vender el producto
+	 * @param cantidad Cantidad que se desea comprar
+	 * @return Devuelve truo o false dependiendo si es posible la compra
+	 */
 	boolean vender(int cantidad) {
 		boolean vender = false;
 		if(cuantosQuedan-cantidad>=0) {
@@ -44,7 +79,10 @@ public class Articulo {
 		}
 		return vender;
 	}
-	
+	/**
+	 * Añade unidades del producto
+	 * @param cantidad Cantidad a añadir dada por el usuario
+	 */
 	void almacenar(int cantidad) {
 		cuantosQuedan += cantidad;
 		System.out.println("Se han añadido " + cantidad + " unidades");
